111f2dda8a77537dc141722ec8e363a51d9e7e47
C5PC5 <C5PC5@C5PC5>
2023-09-27 19:28:56 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿//Структура программы и откладка!
+using System;
+class Programm
+{
+    private static void Main()
+    {
+        int opt1 = 1;
+        int opt2 = 13;
+        int opt3 = 49;
+        string space = " ";
+
+        Console.WriteLine(opt1 + space);
+        Console.WriteLine(opt2 + space);
+        Console.WriteLine(opt3);
+
+        Console.WriteLine(opt1 + space + opt2 + space + opt3);
+        Console.WriteLine( "");
+        int num1 = 1;
+        int num2 = 13;
+        int num3 = 49;
+        string spacespace = " ";
+
+        Console.WriteLine(opt1 + space + space);
+        Console.WriteLine(opt2 + space + space);
+        Console.WriteLine(opt3);
+
+        Console.WriteLine(opt1 + space+ space + opt2 + space + space + opt3);
+
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("Two roads diverged in a yellow wood,");
+        Console.WriteLine("And sorry I could not travel both");
+        Console.WriteLine("And be one traveler, long I stood");
+        Console.WriteLine("And looked down one as far as I could");
+        Console.WriteLine("To where it bent in the undergrowth");
+        Console.WriteLine("");
+    }
+}
+
+// Задание 4 не совсем помню как делать, скорее всего не домонимаю, уроки тоже смотрел ( поставте оценку пожалуйста хотябы за сделанное, так как кроме учеюбы негде делать ДЗ!
